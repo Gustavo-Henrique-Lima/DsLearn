688b7706ebc8aa01dc1726e0e1d40c49dbbeb400
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-22 18:52:32 -0300

    feature: Initial commit.

---
+++ DsLearn/src/main/java/com/gustavonascimento/DsLearn/DsLearnApplication.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.DsLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsLearnApplication.class, args);
+	}
+
+}
---
+++ DsLearn/src/test/java/com/gustavonascimento/DsLearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.DsLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

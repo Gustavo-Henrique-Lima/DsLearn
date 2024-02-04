011287e4362565d220549afffdb5de2bd67445bc
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-02-04 17:46:31 -0300

    feature: Notification Consultation.

--- DsLearn/src/main/java/com/gustavonascimento/DsLearn/repositories/NotificationRepository.java
+++ DsLearn/src/main/java/com/gustavonascimento/DsLearn/repositories/NotificationRepository.java
@@ -3,6 +3,7 @@ package com.gustavonascimento.DsLearn.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gustavonascimento.DsLearn.entities.Notification;
@@ -11,5 +12,9 @@ import com.gustavonascimento.DsLearn.entities.User;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 
-	Page<Notification> findByUser(User user, Pageable pageable);
+	@Query("SELECT obj FROM Notification obj WHERE "
+	        + "(obj.user = :user) AND "
+	        + "(:unreadOnly = false OR obj.read = false) "
+	        + "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
